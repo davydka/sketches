ac600d58dc425767c185d83ac55f2e43592ab342
davydka <davydka@gmail.com>
2024-04-13 23:19:44 -0700

    local LinkItem component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -11,27 +11,36 @@ import {
 } from "@/components/Nav"
 import Icon from '@/assets/icons/comedy-and-tragedy.svg'
 
+const LinkItem = ({href, children}: { href: string, children: React.ReactNode }) => {
+  return (
+    <Link href={href} className='w-full inline-block hover:bg-neutral-800 p-2 rounded'>
+      {children}
+    </Link>
+  )
+}
+
 const Header = ({className}: {
   className?: string;
 }) => {
   return (
     <div className={cn(className)}>
       <Nav>
-
         <NavTrigger className='focus:outline-none'>
           <Icon className='h-6 w-6'/>
         </NavTrigger>
-        <NavContent className='bg-black'>
+        <NavContent sideOffset={12} className='bg-black '>
           <NavLabel>
-            <Link href='/'>
+            <LinkItem href='/'>
               ≼⓪≽
-            </Link>
+            </LinkItem>
           </NavLabel>
+
           <NavSeparator/>
+
           <NavItem>
-            <Link href='/01'>
+            <LinkItem href='/01'>
               01
-            </Link>
+            </LinkItem>
           </NavItem>
         </NavContent>
       </Nav>
