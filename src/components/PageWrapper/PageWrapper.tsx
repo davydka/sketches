e2ce1438f63aaef01892b8c1051f9efc014d6a89
davydka <davydka@gmail.com>
2024-04-13 22:19:44 -0700

    reusable page wrapper

---
+++ src/components/PageWrapper/PageWrapper.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "@/components/Header";
+
+const PageWrapper = ({children}: {
+  children: React.ReactNode,
+}) => {
+  return (
+    <main className="flex p-4 min-h-screen flex-col">
+      <Header className='mb-4'/>
+      {children}
+    </main>
+  )
+}
+
+export default PageWrapper
