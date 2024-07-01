5472f1d6a72cca50985d4942fdf345e008f94da9
davydka <davydka@gmail.com>
2024-06-30 20:46:48 -0700

    prettier updates

--- src/app/01/page.tsx
+++ src/app/01/page.tsx
@@ -1,8 +1,10 @@
-"use client"
+"use client";
 
 // @ts-ignore
-import canvasSketch from "../../../canvas-sketch/lib/canvas-sketch";
-import React, {useCallback, useEffect, useRef} from "react";
+// import canvasSketch from "../../../canvas-sketch/lib/canvas-sketch";
+// @ts-ignore
+import canvasSketch from "canvas-sketch";
+import React, { useCallback, useEffect, useRef } from "react";
 import PageWrapper from "@/components/PageWrapper";
 
 export type CanvasDrawingProps = {
@@ -18,66 +20,69 @@ const HEIGHT = 800;
 export default function Home() {
   const ref = useRef<HTMLCanvasElement | null>(null);
 
-  const draw = useCallback(({context, width, height, playhead, ...rest}: CanvasDrawingProps) => {
-    context.fillStyle = 'black';
-    context.fillRect(0, 0, width, height);
-    context.lineWidth = width * 0.01;
+  const draw = useCallback(
+    ({ context, width, height, playhead, ...rest }: CanvasDrawingProps) => {
+      context.fillStyle = "black";
+      context.fillRect(0, 0, width, height);
+      context.lineWidth = width * 0.01;
 
-    const w = width * 0.10;
-    const h = height * 0.10;
-    const gap = width * 0.03;
-    const ix = width * 0.17;
-    const iy = height * 0.17;
+      const w = width * 0.1;
+      const h = height * 0.1;
+      const gap = width * 0.03;
+      const ix = width * 0.17;
+      const iy = height * 0.17;
 
-    const off = width * 0.02;
+      const off = width * 0.02;
 
-    let x, y;
+      let x, y;
 
-    for (let i = 0; i < 5; i++) {
-      for (let j = 0; j < 5; j++) {
-        x = ix + (w + gap) * i;
-        y = iy + (h + gap) * j;
+      for (let i = 0; i < 5; i++) {
+        for (let j = 0; j < 5; j++) {
+          x = ix + (w + gap) * i;
+          y = iy + (h + gap) * j;
 
-        context.strokeStyle = 'white';
-        context.beginPath();
-        context.rect(x, y, w, h);
-        context.stroke();
-
-        // Draw the text
-        context.fillText(`i:${i}, j:${j}`, x + w / 2, y + h / 2); // Position text in the center of the box
-
-
-        if (Math.random() > 0.5) {
-          context.strokeStyle = 'white';
+          context.strokeStyle = "white";
           context.beginPath();
-          context.rect(x + off / 2, y + off / 2, w - off, h - off);
+          context.rect(x, y, w, h);
           context.stroke();
+
+          // Draw the text
+          context.fillText(`i:${i}, j:${j}`, x + w / 2, y + h / 2); // Position text in the center of the box
+
+          if (Math.random() > 0.5) {
+            context.strokeStyle = "white";
+            context.beginPath();
+            context.rect(x + off / 2, y + off / 2, w - off, h - off);
+            context.stroke();
+          }
         }
       }
-    }
-  }, []);
+    },
+    [],
+  );
 
   useEffect(() => {
-    canvasSketch(() => {
-      return draw
-    }, {
-      dimensions: [WIDTH, HEIGHT],
-      units: "px",
-      resizeCanvas: true,
-      styleCanvas: true,
-      scaleToView: false,
-      canvas: ref.current,
-      animate: true,
-      duration: 3,
-      fps: 30, // export frame rate
-    });
+    canvasSketch(
+      () => {
+        return draw;
+      },
+      {
+        dimensions: [WIDTH, HEIGHT],
+        units: "px",
+        resizeCanvas: true,
+        styleCanvas: true,
+        scaleToView: false,
+        canvas: ref.current,
+        animate: true,
+        duration: 3,
+        fps: 30, // export frame rate
+      },
+    );
   }, [draw]);
 
   return (
     <PageWrapper>
-      <canvas
-        ref={ref}
-      />
+      <canvas ref={ref} />
     </PageWrapper>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-"use client"
+"use client";
 
 // @ts-ignore
-import canvasSketch from "../../canvas-sketch/lib/canvas-sketch";
-import React, {useCallback, useEffect, useRef} from "react";
-import PageWrapper from '@/components/PageWrapper'
+// import canvasSketch from "../../canvas-sketch/lib/canvas-sketch";
+// @ts-ignore
+import canvasSketch from "canvas-sketch";
+import React, { useCallback, useEffect, useRef } from "react";
+import PageWrapper from "@/components/PageWrapper";
 
 export type CanvasDrawingProps = {
   context: CanvasRenderingContext2D;
@@ -18,53 +20,57 @@ const HEIGHT = 800;
 export default function Home() {
   const ref = useRef<HTMLCanvasElement | null>(null);
 
-  const draw = useCallback(({context, width, height, playhead, ...rest}: CanvasDrawingProps) => {
-    // Fill the canvas with pink
-    context.fillStyle = 'black';
-    context.fillRect(0, 0, width, height);
+  const draw = useCallback(
+    ({ context, width, height, playhead, ...rest }: CanvasDrawingProps) => {
+      // Fill the canvas with pink
+      context.fillStyle = "black";
+      context.fillRect(0, 0, width, height);
 
-    // Get a seamless 0..1 value for our loop
-    const t = Math.sin(playhead * Math.PI);
+      // Get a seamless 0..1 value for our loop
+      const t = Math.sin(playhead * Math.PI);
 
-    // Animate the thickness with 'playhead' prop
-    const thickness = Math.max(5, Math.pow(t, 0.55) * width * 0.5);
+      // Animate the thickness with 'playhead' prop
+      const thickness = Math.max(5, Math.pow(t, 0.55) * width * 0.5);
 
-    // Rotate with PI to create a seamless animation
-    const rotation = playhead * Math.PI;
+      // Rotate with PI to create a seamless animation
+      const rotation = playhead * Math.PI;
 
-    // Draw a rotating white rectangle around the center
-    const cx = width / 2;
-    const cy = height / 2;
-    const length = height * 0.5;
-    context.fillStyle = 'white';
-    context.save();
-    context.translate(cx, cy);
-    context.rotate(rotation);
-    context.fillRect(-thickness / 2, -length / 2, thickness, length);
-    context.restore();
-  }, []);
+      // Draw a rotating white rectangle around the center
+      const cx = width / 2;
+      const cy = height / 2;
+      const length = height * 0.5;
+      context.fillStyle = "white";
+      context.save();
+      context.translate(cx, cy);
+      context.rotate(rotation);
+      context.fillRect(-thickness / 2, -length / 2, thickness, length);
+      context.restore();
+    },
+    [],
+  );
 
   useEffect(() => {
-    canvasSketch(() => {
-      return draw
-    }, {
-      dimensions: [WIDTH, HEIGHT],
-      units: "px",
-      resizeCanvas: true,
-      styleCanvas: true,
-      scaleToView: false,
-      canvas: ref.current,
-      animate: true,
-      duration: 3,
-      fps: 30,  // export frame rate
-    });
+    canvasSketch(
+      () => {
+        return draw;
+      },
+      {
+        dimensions: [WIDTH, HEIGHT],
+        units: "px",
+        resizeCanvas: true,
+        styleCanvas: true,
+        scaleToView: false,
+        canvas: ref.current,
+        animate: true,
+        duration: 3,
+        fps: 30, // export frame rate
+      },
+    );
   }, [draw]);
 
   return (
     <PageWrapper>
-      <canvas
-        ref={ref}
-      />
+      <canvas ref={ref} />
     </PageWrapper>
   );
 }
