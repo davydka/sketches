477cf0c7f64f84e540deeb0665777c3a69ad1d7a
davydka <davydka@gmail.com>
2024-10-14 01:55:44 -0700

    global refs provider

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,9 +2,14 @@
 import React from "react";
 import { store } from "@/store";
 import { Provider } from "react-redux";
+import { GlobalRefsProvider } from "@/hooks/useGlobalRefs/useGlobalRefs";
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <GlobalRefsProvider>{children}</GlobalRefsProvider>
+    </Provider>
+  );
 };
 
 export default Providers;
