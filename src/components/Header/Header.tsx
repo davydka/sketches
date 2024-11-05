717c0840917e05828bbde8abede2f8882190fc9a
davydka <davydka@gmail.com>
2024-11-04 19:51:41 -0800

    increase header size

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from "next/link";
-import {cn} from "@/utils/styles";
+import { cn } from "@/utils/styles";
 import {
   Nav,
   NavContent,
@@ -8,47 +8,51 @@ import {
   NavLabel,
   NavSeparator,
   NavTrigger,
-} from "@/components/Nav"
-import Icon from '@/assets/icons/comedy-and-tragedy.svg'
+} from "@/components/Nav";
+import ComedyAndTragedy from "@/assets/icons/comedy-and-tragedy.svg";
 
-const pages = [
-  '01',
-  '02'
-]
+const pages = ["01", "02"];
 
-const LinkItem = ({href, children}: { href: string, children: React.ReactNode }) => {
+const LinkItem = ({
+  href,
+  children,
+}: {
+  href: string;
+  children: React.ReactNode;
+}) => {
   return (
-    <Link href={href} className='w-full inline-block hover:bg-neutral-800 p-2 rounded'>
+    <Link
+      href={href}
+      className="inline-block w-full rounded p-2 hover:bg-neutral-800"
+    >
       {children}
     </Link>
-  )
-}
+  );
+};
 
-const Header = ({className}: {
-  className?: string;
-}) => {
+const Header = ({ className }: { className?: string }) => {
   return (
     <div className={cn(className)}>
       <Nav>
-        <NavTrigger className='focus:outline-none'>
-          <Icon className='h-6 w-6'/>
+        <NavTrigger className="focus:outline-none">
+          <ComedyAndTragedy className="h-10 w-10" />
         </NavTrigger>
-        <NavContent sideOffset={12} className='bg-black ml-4'>
+        <NavContent sideOffset={12} className="ml-4 bg-black">
           <NavLabel>
-            <LinkItem href='/'>
-              ≼⓪≽
-            </LinkItem>
+            <LinkItem href="/">≼⓪≽</LinkItem>
           </NavLabel>
 
-          <NavSeparator/>
+          <NavSeparator />
 
           {pages.map((page) => (
-            <NavItem key={page}><LinkItem href={page}>{page}</LinkItem></NavItem>
+            <NavItem key={page}>
+              <LinkItem href={page}>{page}</LinkItem>
+            </NavItem>
           ))}
         </NavContent>
       </Nav>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
