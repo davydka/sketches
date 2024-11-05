6e7716b5daf43e2a19f5e0c547f79ad2e4f23d17
davydka <davydka@gmail.com>
2024-11-04 19:03:19 -0800

    wireup key shortcut for devtools

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,13 +1,28 @@
 "use client";
 import React from "react";
 import { store } from "@/store";
+import { useAppDispatch } from "@/store/hooks";
 import { Provider } from "react-redux";
 import { GlobalRefsProvider } from "@/hooks/useGlobalRefs/useGlobalRefs";
+import { useHotkeys } from "react-hotkeys-hook";
+import { toggleShowDevtools } from "@/store/slices/devtools.slice";
+
+const KeyCombos = ({ children }: { children: React.ReactNode }) => {
+  const dispatch = useAppDispatch();
+
+  useHotkeys("ctrl+shift+d", () => {
+    dispatch(toggleShowDevtools());
+  });
+
+  return children;
+};
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <Provider store={store}>
-      <GlobalRefsProvider>{children}</GlobalRefsProvider>
+      <KeyCombos>
+        <GlobalRefsProvider>{children}</GlobalRefsProvider>
+      </KeyCombos>
     </Provider>
   );
 };
